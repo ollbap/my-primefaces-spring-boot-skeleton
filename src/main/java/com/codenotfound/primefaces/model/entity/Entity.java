302263b5f7297231f0ef83b19cebcd402c9c4fff
Pablo Morales <ollbap@gmail.com>
2019-02-07 09:04:38 +0100

    First version

---
+++ src/main/java/com/codenotfound/primefaces/model/entity/Entity.java
@@ -0,0 +1,11 @@
+package com.codenotfound.primefaces.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Entity {
+	int id;
+	String name;
+}
