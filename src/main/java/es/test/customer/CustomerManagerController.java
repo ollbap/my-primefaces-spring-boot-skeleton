08df1f95e1d3b8a5b6bf78bcf231870e9a6570fc
Pablo Morales <ollbap@gmail.com>
2019-02-16 14:59:55 +0100

    Added working delete functionality

--- src/main/java/es/test/customer/CustomerManagerController.java
+++ src/main/java/es/test/customer/CustomerManagerController.java
@@ -6,15 +6,11 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.eclipse.jdt.annotation.Nullable;
-
 import es.test.database.Customer;
 import es.test.database.CustomerRepository;
 
 @Named
 public class CustomerManagerController {
-	
-	private @Nullable SelectableListDataModel<Customer> entities = null;
 	
 	private CustomerRepository customerRepository;
 	
@@ -23,25 +19,13 @@ public class CustomerManagerController {
 		this.customerRepository  = customerRepository;
 	}
 	
-	public SelectableListDataModel<Customer> getEntities() {
-		SelectableListDataModel<Customer> lentities = entities;
-		if (lentities == null) {
-			lentities = prepareEntitiesModel();
-			entities = lentities;
-		}
-		return lentities;
-	}
-
-	private SelectableListDataModel<Customer> prepareEntitiesModel() {
+	public List<Customer> getCustomers() {
 		List<Customer> customers = new ArrayList<>();
 		customerRepository.findAll().forEach(c -> customers.add(c));
-		return new SelectableListDataModel<>(customers, c -> ""+c.getId());
+		return customers;
 	}
-	
-	public void deleteEntity() {
-		//TODO mejorar los lios del selector
-		@SuppressWarnings("unchecked")
-		Customer toDelete = ((List<Customer>)entities.getWrappedData()).get(0);
+
+	public void delete(Customer toDelete) {
 		customerRepository.deleteById(toDelete.getId());
 	}
 	
