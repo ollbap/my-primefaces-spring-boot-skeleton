7e7eb13909cc9ce0234ac77e0885e3059bb63afa
Pablo Morales <ollbap@gmail.com>
2019-02-21 23:42:32 +0100

    Validation control enabled

--- src/main/java/es/test/customer/CustomerEditionController.java
+++ src/main/java/es/test/customer/CustomerEditionController.java
@@ -1,5 +1,10 @@
 package es.test.customer;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.PrimeFaces;
+
 import es.test.database.Customer;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,8 +28,15 @@ public class CustomerEditionController {
 	}
 	
 	public void saveEdition() {
+		if (edited.getFirstName().length() > 10 || edited.getLastName().length() > 10) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Name is too long"));
+			return;
+		}
+		
 		manager.getCustomerRepository().save(edited);
 		manager.refreshData();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Modification applied."));
+		PrimeFaces.current().executeScript("PF('editionDialog').hide()");
 	}
 
 	public void createNew() {
