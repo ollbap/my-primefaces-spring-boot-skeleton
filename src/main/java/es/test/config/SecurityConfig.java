8e01acc1dbf011a8d82ca7622546815810f98572
Pablo Morales <ollbap@gmail.com>
2019-02-23 21:09:34 +0100

    Initial spring security configuration

---
+++ src/main/java/es/test/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package es.test.config;
+
+
+import org.eclipse.jdt.annotation.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity(debug=false)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(@Nullable HttpSecurity http) throws Exception {
+		if (http == null) {
+			return;
+		}
+		http
+			//CSRF needs to be disabled for JSF or else, you need some tunning in your JSF forms.
+			.csrf().disable()
+			
+			.authorizeRequests()
+				.antMatchers("/css/**", "/index", "*.xhtml").permitAll()		
+				.antMatchers("/user/**").hasRole("USER")			
+				.and()
+				
+			.formLogin()
+				.loginPage("/login").failureUrl("/login-error");	
+	}
+
+	@SuppressWarnings("static-method")
+	@Autowired
+	public void configureGlobal(@Nullable AuthenticationManagerBuilder auth) throws Exception {
+		if (auth == null) {
+			return;
+		}
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER");
+	}
+}
