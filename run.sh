cd9166029e7ebf2fa6c85bdc955a5af2aa8fb3b9
Pablo Morales <ollbap@gmail.com>
2019-02-27 18:25:50 +0100

    Added different configuration options and examples, different configuration defaults for test and packaged jar

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-#mvn spring-boot:run
-java -jar ./target/jsf-primefaces-spring-boot-0.0.1-SNAPSHOT.jar
+java -jar ./target/*.jar\
+  --spring.datasource.driver-class-name=org.h2.Driver\
+  --spring.datasource.url=jdbc:h2:mem:localhost\;DB_CLOSE_ON_EXIT=FALSE\
+  --spring.datasource.username=admin\
+  --logging.level.org.springframework.web=DEBUG\
+  
+
