facb1ec0e3b6cd86e59e05323552db3259956f4f
Pablo Morales <ollbap@gmail.com>
2019-03-09 13:25:42 +0100

    Fixed identation for ExtendedPrimeExceptionHandlerFactory

--- src/main/java/es/test/config/ExtendedPrimeExceptionHandlerFactory.java
+++ src/main/java/es/test/config/ExtendedPrimeExceptionHandlerFactory.java
@@ -10,32 +10,33 @@ import org.primefaces.application.exceptionhandler.PrimeExceptionHandler;
 import org.primefaces.application.exceptionhandler.PrimeExceptionHandlerFactory;
 
 /**
- * Extended primefaces exception handler factory in order to create a exception handler that redirects
- * to the desired error page.
+ * Extended primefaces exception handler factory in order to create a exception
+ * handler that redirects to the desired error page.
  */
 public class ExtendedPrimeExceptionHandlerFactory extends PrimeExceptionHandlerFactory {
 	private static final String ERROR_PAGE = "error.xhtml";
 
-    public ExtendedPrimeExceptionHandlerFactory(final ExceptionHandlerFactory wrapped) {
-    	super(wrapped);
-    }
+	public ExtendedPrimeExceptionHandlerFactory(final ExceptionHandlerFactory wrapped) {
+		super(wrapped);
+	}
 
-    @Override
-    public ExceptionHandler getExceptionHandler() {
-        return new ExtendedPrimeExceptionHandler(getWrapped().getExceptionHandler());
-    }
-    
-    private static class ExtendedPrimeExceptionHandler extends PrimeExceptionHandler {
+	@Override
+	public ExceptionHandler getExceptionHandler() {
+		return new ExtendedPrimeExceptionHandler(getWrapped().getExceptionHandler());
+	}
+
+	private static class ExtendedPrimeExceptionHandler extends PrimeExceptionHandler {
 
 		public ExtendedPrimeExceptionHandler(ExceptionHandler wrapped) {
 			super(wrapped);
 		}
-		
-	    @Override
-		protected String evaluateErrorPage(@SuppressWarnings("null") Map<String, String> errorPages, @Nullable Throwable rootCause) {
-	    	return ERROR_PAGE;
-	    }
-    	
-    }
+
+		@Override
+		protected String evaluateErrorPage(@SuppressWarnings("null") Map<String, String> errorPages,
+				@Nullable Throwable rootCause) {
+			return ERROR_PAGE;
+		}
+
+	}
 
 }
